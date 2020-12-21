d1bfc98afffe8df0e86a836a9e1f28dc3fa07a5b
David Peicho <david.peicho@gmail.com>
2020-12-22 00:35:40 +0100

    albedo_math: add clamp

--- crates/albedo_math/src/lib.rs
+++ crates/albedo_math/src/lib.rs
@@ -1,14 +1,36 @@
 mod aabb;
 
+use std::ops::{Index, IndexMut};
+use glam::Vec3;
+
 pub use aabb::{AABB};
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Size3D {
     pub x: f32,
     pub y: f32,
     pub z: f32,
 }
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Axis3D {
-    X,
-    Y,
-    Z
+    X = 0,
+    Y = 1,
+    Z = 2
 }
 
+impl Index<Axis3D> for Vec3 {
+    type Output = f32;
+
+    fn index(&self, axis: Axis3D) -> &f32 {
+        &self[axis as usize]
+    }
+}
+
+pub fn clamp<T: PartialOrd>(val: T, min: T, max: T) -> T {
+    if val < min {
+        min
+    } else if val > max {
+        max
+    } else {
+        val
+    }
+}
