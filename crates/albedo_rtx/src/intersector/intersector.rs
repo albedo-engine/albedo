6a428240fc928c2119f64c9c724be8634e8e451e
David Peicho <david.peicho@gmail.com>
2021-03-05 15:41:02 +0100

    ingtersector: wip

--- crates/albedo_rtx/src/intersector/intersector.rs
+++ crates/albedo_rtx/src/intersector/intersector.rs
@@ -1,10 +1,19 @@
 pub struct GPUIntersector {
-    // bind_group: wgpu::BindGroup,
+    bind_group: wgpu::BindGroup,
 }
 
 impl GPUIntersector {
 
-    pub fn new(device: &wgpu::Device) -> GPUIntersector {
+    pub fn new(
+        device: &wgpu::Device,
+        nodes: &wgpu::Buffer,
+        indices: &wgpu::Buffer,
+        vertices: &wgpu::Buffer,
+        lights: &wgpu::Buffer,
+        rays: &wgpu::Buffer,
+        scene_info: &wgpu::Buffer
+    ) -> GPUIntersector {
+
         GPUIntersector {
 
         }
