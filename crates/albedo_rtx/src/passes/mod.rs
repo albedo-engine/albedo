5c527d776a53e0fb58fc17febbe52c75b083dda6
David Peicho <david.peicho@gmail.com>
2022-02-22 21:21:32 +0100

    add debug pass

--- crates/albedo_rtx/src/passes/mod.rs
+++ crates/albedo_rtx/src/passes/mod.rs
@@ -3,9 +3,11 @@ mod blit_pass;
 mod intersector;
 mod radiance_estimator;
 mod ray_generator;
+mod debug_bvh_pass;
 
 pub use accumulation::AccumulationPass;
 pub use blit_pass::BlitPass;
 pub use intersector::GPUIntersector;
 pub use radiance_estimator::GPURadianceEstimator;
 pub use ray_generator::GPURayGenerator;
+pub use debug_bvh_pass::BVHDebugPass;
