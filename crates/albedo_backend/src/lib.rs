b9013fa9e61d125bbeceefaadf0815f3f8a89f2f
David Peicho <david.peicho@gmail.com>
2022-03-28 23:11:05 +0200

    albedo_backend: add bytes_per_pixel to alignment

--- crates/albedo_backend/src/lib.rs
+++ crates/albedo_backend/src/lib.rs
@@ -7,15 +7,13 @@ pub use buffer::{GPUBuffer, UniformBuffer};
 pub use compute_pass::{ComputePass, ComputePassDescriptor};
 
 pub struct Alignment2D {
-    unpadded_bytes_per_row: usize,
-    padded_bytes_per_row: usize,
+    pub unpadded_bytes_per_row: usize,
+    pub padded_bytes_per_row: usize,
 }
 
 impl Alignment2D {
-    pub fn new(width: usize, align: usize) -> Self {
-        let bytes_per_pixel = std::mem::size_of::<u32>();
+    pub fn new(width: usize, bytes_per_pixel: usize, align: usize) -> Self {
         let unpadded_bytes_per_row = width * bytes_per_pixel;
-        let align = wgpu::COPY_BYTES_PER_ROW_ALIGNMENT as usize;
         let padded_bytes_per_row_padding = (align - unpadded_bytes_per_row % align) % align;
         let padded_bytes_per_row = unpadded_bytes_per_row + padded_bytes_per_row_padding;
         Self {
@@ -24,15 +22,19 @@ impl Alignment2D {
         }
     }
 
-    pub fn texture_buffer_copy(width: usize) -> Self {
-        Self::new(width, wgpu::COPY_BYTES_PER_ROW_ALIGNMENT as usize)
+    pub fn texture_buffer_copy(width: usize, bytes_per_pixel: usize) -> Self {
+        Self::new(
+            width,
+            bytes_per_pixel,
+            wgpu::COPY_BYTES_PER_ROW_ALIGNMENT as usize,
+        )
     }
 
-    pub fn bytes() -> usize {
+    pub fn bytes(&self) -> usize {
         self.unpadded_bytes_per_row
     }
 
-    pub fn padded_bytes() -> usize {
+    pub fn padded_bytes(&self) -> usize {
         self.padded_bytes_per_row
     }
 }
