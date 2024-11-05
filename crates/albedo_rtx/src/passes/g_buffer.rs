06133f77fa82a4679562358b2a7cdd103a0735c4
David Peicho <david.peicho@wonderlandengine.com>
2024-11-05 23:36:48 +0100

    albedo_rtx: Migrate gbuffer.comp

--- crates/albedo_rtx/src/passes/g_buffer.rs
+++ crates/albedo_rtx/src/passes/g_buffer.rs
@@ -1,4 +1,8 @@
+use std::borrow::{Borrow, Cow};
+
+use albedo_backend::data::ShaderCache;
 use albedo_backend::gpu;
+use wgpu::naga;
 
 use crate::macros::path_separator;
 use crate::{get_dispatch_size, uniforms};
@@ -17,8 +21,9 @@ impl GBufferPass {
 
     pub fn new(
         device: &wgpu::Device,
+        processor: &mut ShaderCache,
         geometry_layout: &crate::RTGeometryBindGroupLayout,
-        source: Option<wgpu::ShaderModuleDescriptor>,
+        source: Option<&str>,
     ) -> Self {
         let frame_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
@@ -66,18 +71,25 @@ impl GBufferPass {
             }],
         });
 
-        let shader = match source {
-            None => device.create_shader_module(wgpu::include_spirv!(concat!(
-                "..",
-                path_separator!(),
-                "shaders",
-                path_separator!(),
-                "spirv",
-                path_separator!(),
-                "gbuffer.comp.spv"
-            ))),
-            Some(v) => device.create_shader_module(v),
-        };
+        let source = source.unwrap_or(include_str!(concat!(
+            "..",
+            path_separator!(),
+            "..",
+            path_separator!(),
+            "shaders",
+            path_separator!(),
+            "gbuffer.comp"
+        )));
+        let source = processor.compile(source).unwrap();
+
+        let shader: wgpu::ShaderModule = device.create_shader_module(wgpu::ShaderModuleDescriptor{
+            label: Some("GBuffer Shader"),
+            source: wgpu::ShaderSource::Glsl {
+                shader: Cow::Borrowed(source.borrow()),
+                stage: naga::ShaderStage::Compute,
+                defines: naga::FastHashMap::default()
+            }
+        });
 
         let pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
             label: Some("Gbuffer Pipeline"),
