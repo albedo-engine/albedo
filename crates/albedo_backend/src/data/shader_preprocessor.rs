5e85c32e4ee98b61197bea71b94a6e82a67d114f
David Peicho <david.peicho@wonderlandengine.com>
2024-11-10 20:14:26 +0100

    albedo_backend: Create naga::Module in ShaderCache

--- crates/albedo_backend/src/data/shader_preprocessor.rs
+++ crates/albedo_backend/src/data/shader_preprocessor.rs
@@ -1,15 +1,34 @@
-use std::{collections::HashMap, fmt::Debug, str::Lines, usize};
+use std::{collections::HashMap, fmt::Debug};
+use wgpu::naga;
 
 pub enum ShaderError {
     SyntaxError,
-    Missing
+    Missing(String)
+}
+
+#[derive(Debug)]
+pub enum CompileError {
+    Preprocessor(ShaderError),
+    Module(naga::front::glsl::ParseErrors),
+}
+
+impl From<naga::front::glsl::ParseErrors> for CompileError {
+    fn from(value: naga::front::glsl::ParseErrors) -> Self {
+        CompileError::Module(value)
+    }
+}
+
+impl From<ShaderError> for CompileError {
+    fn from(value: ShaderError) -> Self {
+        CompileError::Preprocessor(value)
+    }
 }
 
 impl Debug for ShaderError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::SyntaxError => write!(f, "SyntaxError"),
-            Self::Missing => write!(f, "Missing"),
+            Self::Missing(import) => write!(f, "Missing import: '{}'", import),
         }
     }
 }
@@ -32,7 +51,7 @@ impl ShaderCache {
         }
     }
 
-    pub fn compile(&mut self, source: &str) -> Result<String, ShaderError> {
+    pub fn compile(&self, source: &str) -> Result<String, ShaderError> {
         let lines = source.lines();
         let mut buf = String::new();
         for line in lines {
@@ -50,10 +69,33 @@ impl ShaderCache {
             };
             let name = &include[1..end+1];
             let Some(content) = self.imports.get(name) else {
-                return Err(ShaderError::Missing);
+                return Err(ShaderError::Missing(name.to_string()));
             };
             buf.extend([content, "\n"]);
         }
         Ok(buf)
+    }
+
+    pub fn compile_compute(&self, source: &str) -> Result<naga::Module, CompileError> {
+        self.compile_module(source, naga::ShaderStage::Compute)
+    }
+    pub fn compile_fragment(&self, source: &str) -> Result<naga::Module, CompileError> {
+        self.compile_module(source, naga::ShaderStage::Fragment)
+    }
+    pub fn compile_vertex(&self, source: &str) -> Result<naga::Module, CompileError> {
+        self.compile_module(source, naga::ShaderStage::Vertex)
+    }
+
+    pub fn compile_module(&self, source: &str, stage: naga::ShaderStage) -> Result<naga::Module, CompileError> {
+        let source = self.compile(source)?;
+        let module = naga::front::glsl::Frontend::default()
+            .parse(
+                &naga::front::glsl::Options {
+                    stage,
+                    defines: Default::default(),
+                },
+                &source,
+            )?;
+        Ok(module)
     }
 }
